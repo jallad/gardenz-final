3fc8c2040ddef672ab7092be78dd2f18fd5be3e6
Ahmad Jallad <ajallad@outlook.com>
2021-04-15 19:44:02 -0400

    added authentication system

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
-    flash[:notice] = "Logged in successfully"
-    flash[:alert] = "Invalid email or password"
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get '/login', to: 'sessions#new', as: 'login'
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
+
   get 'home/index'
+  get 'sign_up', to: 'registration#new'
+  post 'sign_up', to: 'registration#create'
 
   root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
